2c1bf9f89121506e2146106f24ac27873b758ddc
albertschool <albert.school2015@gmail.com>
2021-01-10 13:07:27 +0200

    Initial commit for ImageView Demo

---
+++ app/src/main/java/com/videxedge/imgview/MainActivity.java
@@ -0,0 +1,27 @@
+package com.videxedge.imgview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView iV;
+    int twoimg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iV = (ImageView) findViewById(R.id.iV);
+
+        twoimg = R.drawable.two;
+    }
+
+    public void showimg(View view) {
+        iV.setImageResource(twoimg);
+    }
+}
